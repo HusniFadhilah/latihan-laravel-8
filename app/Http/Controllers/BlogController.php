356d206b15fca8db03fe2cef382f4f7d1e94c099
HusniFadhilah <husnfadhilah62@gmail.com>
2020-11-04 11:17:32 +0700

    Membuat method edit & update blogcontroller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -57,4 +57,64 @@ class BlogController extends Controller
             return redirect()->route('blog.index')->with(['error' => 'Data Gagal Disimpan!']);
         }
     }
+
+    /**
+     * edit
+     *
+     * @param  mixed $blog
+     * @return void
+     */
+    public function edit(Blog $blog)
+    {
+        return view('blog.edit', compact('blog'));
+    }
+
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $blog
+     * @return void
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $this->validate($request, [
+            'title'     => 'required',
+            'content'   => 'required'
+        ]);
+
+        //get data Blog by ID
+        $blog = Blog::findOrFail($blog->id);
+
+        if ($request->file('image') == "") {
+
+            $blog->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        } else {
+
+            //hapus old image
+            Storage::disk('local')->delete('public/blogs/' . $blog->image);
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/blogs', $image->hashName());
+
+            $blog->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        if ($blog) {
+            //redirect dengan pesan sukses
+            return redirect()->route('blog.index')->with(['success' => 'Data Berhasil Diupdate!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('blog.index')->with(['error' => 'Data Gagal Diupdate!']);
+        }
+    }
 }
