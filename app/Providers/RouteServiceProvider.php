4163063629fa27988da7f9afe6e35a4432e487a9
HusniFadhilah <husnfadhilah62@gmail.com>
2020-11-04 10:51:04 +0700

    Menambahkan namespace di routeserviceprovider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    protected $namespace = 'App\Http\Controllers';
+
     /**
      * The path to the "home" route for your application.
      *
@@ -17,6 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
+
     public const HOME = '/home';
 
     /**
