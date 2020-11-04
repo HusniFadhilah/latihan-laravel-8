6d724722109ffedaa3d58f9d9f9bba6f5daefdfa
HusniFadhilah <husnfadhilah62@gmail.com>
2020-11-04 10:46:06 +0700

    Membuat controller blog

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::latest()->paginate(10);
+        return view('blog.index', compact('blogs'));
+    }
+}
